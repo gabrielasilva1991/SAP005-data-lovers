c6f24e507088a2007eae55a64d837250c920b946
gabrielasilva1991 <gabriela.silva1991@outlook.com>
2020-11-28 15:51:17 -0200

    cards criados

--- src/data.js
+++ src/data.js
@@ -1,12 +1,26 @@
-// estas funciones son de ejemplo
 
-export const example = () => {
+export const buscarNumero = (data, num) => {
   return 'example';
 };
 
-export const anotherExample = () => {
+export const buscarTipo = (data, type) => {
+  return 'example';
+};
+
+export const buscarRaridade = (date, raridade) => {
   return 'OMG';
 };
 
+export const buscarEstatisticas = (data, ) => {
+  return 'example';
+};
 
-//funcões que faz os filtros
+export const buscarOrdemCrescente = (data, ) => {
+  return 'example';
+};
+
+export const buscarOrdemDecrescente = (data, ) => {
+  return 'example';
+};
+
+//funcões que faz os filtros e ordenação A-Z
