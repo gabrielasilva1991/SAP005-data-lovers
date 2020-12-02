fca841bc4174d742816d906c911f995d4eb7db4f
gabrielasilva1991 <gabriela.silva1991@outlook.com>
2020-12-02 17:23:32 -0200

    filtro busca por nome

--- data.js
+++ data.js
@@ -1,35 +1,19 @@
 export const inputNome = (buscarNome, array) => {
-  return array.filter(pokemon => pokemon.name.startsWith(buscarNome)); //retorna se a string passada está no começo 
+  return array.filter(pokemon => pokemon.name.startsWith(buscarNome));
+  // Quando começa a escrever ele vai mostrando sugestoes que começam com a letra
+  // O método startsWith() determina se uma string começa com os caracteres especificados, retornando true ou false.
+  // Esse método é case-sensitive (difere maiúsculas de minúsculas, e vice-versa). 
 };
 
-export const selectNumero = (buscarNumero, array) => {
-  return array.filter(pokemon => pokemon.num.includes(buscarNumero)); // checar se o parametro está em algum lugar da string
-};
-
-export const selectTipo = (buscarTipo, array) => {
-  return array.filter(pokemon => pokemon.type.includes(buscarTipo));
-};
-
-
-export const calculoTipo = (buscarTipo, array) => {
-  const calculo = pokemons.filter(pokemon => pokemon.type.includes(buscarTipo));
-  
-};
-   
-
-export const selectRaridade = (buscarRaridade, array) => {
-  return array.filter(pokemon => pokemon.pokemon-rarity.includes(buscarRaridade));
-};
-
-export const ordenarCrescente = (ordenarCrescente, array ) => {
+export const selectNumeroCrescente = (buscarNumero, array) => {
   array.sort(function (a, b) {
     if (a.name > b.name) return 1;
     if (a.name < b.name) return -1;
     return 0
   });
 };
-
-export const ordenarDecrescente = (ordenarDecrescente, array ) => { 
+  
+export const selectNumeroDecrescente = (buscarNumero, array) => {
   array.sort(function (a, b) {
     if (a.name > b.name) return -1;
     if (a.name < b.name) return 1;
@@ -37,7 +21,33 @@ export const ordenarDecrescente = (ordenarDecrescente, array ) => {
   });
 };
 
+export const selectTipo = (buscarTipo, array) => {
+  return array.filter(pokemon => pokemon.type.includes(buscarTipo)); // checar se um array/objeto possui determinado valor/elemento.
+};
 
+
+export const calculoTipo = (buscarTipo, array) => {
+  const calculo = pokemons.filter(pokemon => pokemon.type.includes(buscarTipo));
+  
+};
+
+export const selectNomeCrescente = (ordenarCrescente, array ) => {
+  array.sort(function (a, b) {
+    if (a.name > b.name) return 1;
+    if (a.name < b.name) return -1;
+    return 0
+  });
+};
+
+export const selectNomeDecrescente = (ordenarDecrescente, array ) => { 
+  array.sort(function (a, b) {
+    if (a.name > b.name) return -1;
+    if (a.name < b.name) return 1;
+    return 0
+  });
+};
+
+//funções que fazem ordenação e filtro
 //return data.filter((pokemon) => pokemon.type.includes(buscarTipo));
 //return agua = data.filter((pokemon) => pokemon.type === 'water'));
 //Água Dragão Elétrico Fada Fantasma Fogo Gelo Inseto Lutador Metálico Normal Noturno Pedra Planta Psíquico Terrestre Venenoso Voador
