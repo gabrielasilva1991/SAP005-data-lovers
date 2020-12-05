e028ceaf02ad802b518242377265538f0eb90490
gabrielasilva1991 <gabriela.silva1991@outlook.com>
2020-12-05 02:01:18 -0200

    adaptação de layout

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,64 @@
-import { inputNome, menuSelectTipo, selectNumeroCrescente, selectNumeroDecrescente, selectNomeCrescente, selectNomeDecrescente } from '../src/data.js';
+import { inputNome, menuSelectTipo } from '../src/data.js';
 import data from './mock.js';
 
 describe('inputNome', () => {
-  it('Deve retornar', () => {
+  it('Deve ser uma função', () => {
     expect(typeof inputNome).toBe('function');
   });
 
-  it('Deve retornar pokemons com nome "bulba"', () => { 
-    expect(inputNome("bulba", data.pokemon)).toBe('example');
+  it('Deve retornar pokemons que iniciam o nome com "gol"', () => { 
+    expect(inputNome("gol", data.pokemon)).toStricEqual([{
+      "num": "042",
+      "name": "golbat",
+      "type": [
+        "poison",
+        "flying"
+      ],
+
+      "num": "055",
+      "name": "golduck",
+      "type": [
+        "water"
+      ],
+
+      "num": "076",
+      "name": "golem",
+      "type": [
+        "rock",
+        "ground"
+      ],   
+  
+    }]);
   });
 });
 
-
-describe('anotherExample', () => {
-  it('is a function', () => {
+describe('menuSelectTipo', () => {
+  it('Deve ser uma função', () => {
     expect(typeof menuSelectTipo).toBe('function');
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it('Deve retornar pokemons que são do tipo "water"', () => {
+    expect(menuSelectTipo("water", data.pokemon)).toStricEqual([{
+      "num": "055",
+      "name": "golduck",
+      "type": [
+        "water"
+      ],
+
+      "num": "183",
+      "name": "marill",
+      "type": [
+        "water",
+        "fairy"
+      ],
+
+      "num": "194",
+      "name": "wooper",
+      "type": [
+        "water",
+        "ground"
+      ],
+    
+    }]);
   });
 });
